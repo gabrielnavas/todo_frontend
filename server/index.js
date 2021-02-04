8b4c208d244974e4029275fd8d677308200ab1cd
navasi <gabrielnavas@email.com>
2021-02-04 16:04:02 -0300

    test deploy heroku

--- server/index.js
+++ server/index.js
@@ -4,14 +4,14 @@ const path = require('path')
 const app = express()
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, '/build')))
+app.use(express.static(path.join(__dirname, '../build')))
 
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
-  res.sendFile(path.join(path.resolve(__dirname, '/build/index.html')))
+  res.sendFile(path.join(path.resolve(__dirname, '../build/index.html')))
 })
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3000
 app.listen(port)
 
 console.log('App is listening on port ' + port)
